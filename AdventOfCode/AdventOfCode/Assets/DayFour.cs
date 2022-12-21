aa1995a241c3f45def20f7160e6587171fa72fc1
Chris Bolton <c.bolton@vtime.net>
2022-12-21 15:22:41 +0000

    Day Four part one

---
+++ AdventOfCode/AdventOfCode/Assets/DayFour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayFour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int totalCount = 0;
+    void Start()
+    {
+        foreach (var PairOfRanges in GetFileContents())
+        {
+            string[] Ranges = PairOfRanges.Split(',');
+            //in Ranges[0] is the first range, in Ranges[1] is the second range in the format "1-3". If one of the ranges fully contains the other, then the total count is increased by 1.
+            string[] Range1 = Ranges[0].Split('-');
+            string[] Range2 = Ranges[1].Split('-');
+            int Range1Min = int.Parse(Range1[0]);
+            int Range1Max = int.Parse(Range1[1]);
+            int Range2Min = int.Parse(Range2[0]);
+            int Range2Max = int.Parse(Range2[1]);
+            if (Range1Min <= Range2Min && Range1Max >= Range2Max)
+            {
+                totalCount++;
+            }
+            else if (Range1Min >= Range2Min && Range1Max <= Range2Max)
+            {
+                totalCount++;
+            }
+        }
+        
+        Debug.Log(totalCount);
+    }
+
+    string[] GetFileContents()
+    {
+        string[] input = System.IO.File.ReadAllText(Application.streamingAssetsPath + "/input4.txt")
+            .Split(new string[] { "\n" }, System.StringSplitOptions.None);
+        return input;
+    }
+
+}
