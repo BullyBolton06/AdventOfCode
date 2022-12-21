186bbfc3693d91ede1e6648f54811d81e5151e0f
Chris Bolton <c.bolton@vtime.net>
2022-12-21 15:27:37 +0000

    Day Four part 2

--- AdventOfCode/AdventOfCode/Assets/DayFour.cs
+++ AdventOfCode/AdventOfCode/Assets/DayFour.cs
@@ -11,21 +11,22 @@ public class DayFour : MonoBehaviour
         foreach (var PairOfRanges in GetFileContents())
         {
             string[] Ranges = PairOfRanges.Split(',');
-            //in Ranges[0] is the first range, in Ranges[1] is the second range in the format "1-3". If one of the ranges fully contains the other, then the total count is increased by 1.
+            //in Ranges[0] is the first range, in Ranges[1] is the second range in the format "1-3". If the ranges overlap at all, the total count is increased by 1.
             string[] Range1 = Ranges[0].Split('-');
             string[] Range2 = Ranges[1].Split('-');
             int Range1Min = int.Parse(Range1[0]);
             int Range1Max = int.Parse(Range1[1]);
             int Range2Min = int.Parse(Range2[0]);
             int Range2Max = int.Parse(Range2[1]);
-            if (Range1Min <= Range2Min && Range1Max >= Range2Max)
+            if (Range1Min <= Range2Min && Range1Max >= Range2Min)
             {
                 totalCount++;
             }
-            else if (Range1Min >= Range2Min && Range1Max <= Range2Max)
+            else if (Range2Min <= Range1Min && Range2Max >= Range1Min)
             {
                 totalCount++;
             }
+            
         }
         
         Debug.Log(totalCount);
